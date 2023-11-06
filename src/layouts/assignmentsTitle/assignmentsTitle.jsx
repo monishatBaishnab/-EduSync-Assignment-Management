96a3efcd77b23fac96020541889f9a44900ac31f
monishat <monishat@gmail.com>
2023-11-06 19:06:48 +0600

    Implement filter and sort functionality on the assignments page

--- src/layouts/assignmentsTitle/assignmentsTitle.jsx
+++ src/layouts/assignmentsTitle/assignmentsTitle.jsx
@@ -1,20 +1,43 @@
 import { Option, Select, Typography } from "@material-tailwind/react";
 import PropTypes from 'prop-types';
+import { useEffect, useState } from "react";
 
-const AssignmentsTitle = ({ title }) => {
-    const handleSeelct = (value) => {
-        console.log(value);
+const AssignmentsTitle = ({ title, setUrl }) => {
+    const [filterValue, setFilterValue] = useState('');
+    const [sortValue, setSortValue] = useState('');
+
+    useEffect(() => {
+        const url = `/assignments?level=${filterValue}&sort=mark&sortOrder=${sortValue}`;
+        setUrl(url);
+    }, [filterValue, sortValue, setUrl])
+
+    const handleFilter = (value) => {
+        setFilterValue(value);
     }
+    const handleSort = (value) => {
+        setSortValue(value);
+    }
+
+
+
     return (
         <div className="container border-b py-5">
             <div className="flex justify-between flex-wrap">
                 <Typography className="basis-full text-center mb-5 sm:mb-0 sm:text-start sm:basis-1/2" variant="h4">{title}</Typography>
-                <div className="basis-full sm:basis-1/2 flex justify-center">
+                <div className="basis-full sm:basis-1/2 flex justify-end">
                     <div className="w-[100px] md:ml-auto">
-                        <Select containerProps={{ className: '!min-w-[100px]' }} onChange={handleSeelct} label="Label">
+                        <Select containerProps={{ className: '!min-w-[100px]' }} onChange={handleFilter} label="Filter">
+                            <Option value="">All</Option>
                             <Option value="easy">Easy</Option>
                             <Option value="medium">Medium</Option>
                             <Option value="hard">Hard</Option>
+                        </Select>
+                    </div>
+                    <div className="w-[200px] ml-5">
+                        <Select containerProps={{ className: '!min-w-[200px]' }} onChange={handleSort} label="Sort">
+                            <Option value="">No Filter</Option>
+                            <Option value="asc">Ascending</Option>
+                            <Option value="desc">Descending</Option>
                         </Select>
                     </div>
                 </div>
@@ -25,6 +48,7 @@ const AssignmentsTitle = ({ title }) => {
 
 AssignmentsTitle.propTypes = {
     title: PropTypes.string,
+    setUrl: PropTypes.func,
 }
 
 export default AssignmentsTitle;
--- src/pages/Assignments/Assignments.jsx
+++ src/pages/Assignments/Assignments.jsx
@@ -3,22 +3,23 @@ import useAxios from "../../hooks/useAxios";
 import AssignmentsContainer from "../../layouts/assignmentsContainer/AssignmentsContainer";
 import AssignmentsTitle from "../../layouts/assignmentsTitle/assignmentsTitle";
 import Pagination from "../../layouts/pagination/Pagination";
+import { useState } from "react";
 
 
 const Assignments = () => {
     // const assignments = useFetchAssignments('/assignments');
     const axios = useAxios();
+    const [url, setUrl] = useState('/assignments');
     const getAssignments = async () => {
-        const res = await axios.get('/assignments');
+        const res = await axios.get(url);
         return res;
     }
 
-    const query = useQuery({queryKey: ['assignments'], queryFn: getAssignments})
-   
-    // console.log(assignments);
+    const query = useQuery({queryKey: ['assignments', url], queryFn: getAssignments});
+    
     return (
         <div className="bg-[#f7f8f9]">
-            <AssignmentsTitle title='All Assignments' />
+            <AssignmentsTitle setUrl={setUrl} title='All Assignments' />
             <AssignmentsContainer query={query} />
             <Pagination />
         </div>
