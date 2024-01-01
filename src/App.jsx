10a1642b855c0964a273a0863ce55f94859018b9
Monishat Baishnab <baishnabmonishat@gmail.com>
2024-01-01 23:54:10 +0600

    Reorganized menu and footer folders, added reusable components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Outlet, useLocation, useNavigation } from "react-router-dom"
-import Navigation from "./layouts/comon/navigation/Navigation"
-import Footer from "./layouts/comon/footer/Footer"
 import Loading from "./pages/Loading/Loading";
 import { useEffect } from "react";
+import Navigation from "./components/SharedComponents/Navigation/Navigation";
+import Footer from "./components/SharedComponents/Footer";
 
 function App() {
   const { state } = useNavigation();
